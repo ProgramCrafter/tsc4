c03f62d8af34c69c0b153e7953e9bea03679dbf4
Andrey <pythonhunter146@gmail.com>
2023-08-10 12:35:50 +0500

    Changed test system, tested task 1

--- wrappers/Task5.compile.ts
+++
@@ -1,6 +0,0 @@
-import { CompilerConfig } from '@ton-community/blueprint';
-
-export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['contracts/imports/stdlib.fc', 'contracts/5.fc'],
-};
